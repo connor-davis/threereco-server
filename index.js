3ea6ac17c5361bce59fddeadde6a46651924915c
Connor Davis <19422@kloofhigh.co.za>
2022-02-15 08:41:15 +0200

    use key and cert

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ logger.info(`OP MODE: ${devmode ? 'DEV' : 'PROD'}`);
 if (!devmode) {
   https = require('https').createServer(
     {
-      cert: fs.readFileSync('./ssl/server.cert'),
-      key: fs.readFileSync('./ssl/server.key'),
+      cert: fs.readFileSync('./ssl/cert.pem'),
+      key: fs.readFileSync('./ssl/key.pem'),
     },
     app
   );
