ec2ecbc247a51f3f7f020bdb8d8efc06c0e982f1
Connor Davis <19422@kloofhigh.co.za>
2022-03-22 10:29:25 +0200

    Update transactions to add and subtract stock

--- api/transactions/addTransaction.js
+++ api/transactions/addTransaction.js
@@ -12,52 +12,31 @@ router.post('/', async (request, response) => {
     m1.milliseconds() +
     1000 * (m1.seconds() + 60 * (m1.minutes() + 60 * m1.hours()));
 
-  let devmode = process.env.DEV_MODE === "true";
+  let devmode = process.env.DEV_MODE === 'true';
   let connection = await r.connect({
-      host: devmode ? 'localhost' : process.env.RETHINK,
-      port: 28015,
-      user: "admin",
-      password: process.env.ROOT_PASSWORD
-    });
-
-  console.log(body);
-
-  let transactionObject =
-    body.type === 'purchase'
-      ? {
-          user: request.user.id,
-          purchaser: request.user,
-          material: body.material,
-          weight: body.weight,
-          seller: body.seller,
-          price: body.price,
-          type: body.type,
-          date: Date.now(),
-        }
-      : {
-          user: request.user.id,
-          purchaser: body.purchaser,
-          material: body.material,
-          weight: body.weight,
-          seller: request.user,
-          price: body.price,
-          type: body.type,
-          date: Date.now(),
-        };
+    host: devmode ? 'localhost' : process.env.RETHINK,
+    port: 28015,
+    user: 'admin',
+    password: process.env.ROOT_PASSWORD,
+  });
 
   r.db('threereco')
-    .table('userTransactions')
-    .insert(transactionObject)
-    .run(connection, async (error, result) => {
-      let m2 = moment();
-      let operationEnded =
-        m2.milliseconds() +
-        1000 * (m2.seconds() + 60 * (m2.minutes() + 60 * m2.hours()));
-
+    .table('materials')
+    .get({
+      user: request.user.id,
+    })
+    .update({
+      stock:
+        body.type === 'purchase'
+          ? r.row('stock').add(body.weight)
+          : r.row('stock').sub(body.weight),
+    })
+    .run(connection, (error, result) => {
       if (error) {
-        response
-          .status(500)
-          .json({ message: 'Error while creating new user purchase.', error });
+        response.status(500).json({
+          message: 'Error while creating new user transaction.',
+          error,
+        });
 
         logger.error(error);
 
@@ -65,28 +44,101 @@ router.post('/', async (request, response) => {
           `Operation took ${operationEnded - operationStarted}ms.`
         );
       } else {
-        if (result.inserted >= 1) {
-          response.status(200).json({
-            message: 'Created new user purchase successfully.',
-            data: transactionObject,
+        r.db('threereco')
+          .table('materials')
+          .get({
+            user: body.type === 'purchase' ? body.seller.id : body.purchaser.id,
+          })
+          .update({
+            stock:
+              body.type === 'purchase'
+                ? r.row('stock').add(body.weight)
+                : r.row('stock').sub(body.weight),
+          })
+          .run(connection, (error, result) => {
+            if (error) {
+              response.status(500).json({
+                message: 'Error while creating new user transaction.',
+                error,
+              });
+
+              logger.error(error);
+
+              return logger.info(
+                `Operation took ${operationEnded - operationStarted}ms.`
+              );
+            } else {
+              let transactionObject =
+                body.type === 'purchase'
+                  ? {
+                      user: request.user.id,
+                      purchaser: request.user,
+                      material: body.material,
+                      weight: body.weight,
+                      seller: body.seller,
+                      price: body.price,
+                      type: body.type,
+                      date: Date.now(),
+                    }
+                  : {
+                      user: request.user.id,
+                      purchaser: body.purchaser,
+                      material: body.material,
+                      weight: body.weight,
+                      seller: request.user,
+                      price: body.price,
+                      type: body.type,
+                      date: Date.now(),
+                    };
+
+              r.db('threereco')
+                .table('userTransactions')
+                .insert(transactionObject)
+                .run(connection, async (error, result) => {
+                  let m2 = moment();
+                  let operationEnded =
+                    m2.milliseconds() +
+                    1000 *
+                      (m2.seconds() + 60 * (m2.minutes() + 60 * m2.hours()));
+
+                  if (error) {
+                    response.status(500).json({
+                      message: 'Error while creating new user transaction.',
+                      error,
+                    });
+
+                    logger.error(error);
+
+                    return logger.info(
+                      `Operation took ${operationEnded - operationStarted}ms.`
+                    );
+                  } else {
+                    if (result.inserted >= 1) {
+                      response.status(200).json({
+                        message: 'Created new user transaction successfully.',
+                        data: transactionObject,
+                      });
+
+                      logger.success('Created a new user transaction.');
+
+                      return logger.info(
+                        `Operation took ${operationEnded - operationStarted}ms.`
+                      );
+                    } else {
+                      response.status(500).json({
+                        message: 'Error while creating new user transaction.',
+                      });
+
+                      logger.error('Could not create new user transaction.');
+
+                      return logger.info(
+                        `Operation took ${operationEnded - operationStarted}ms.`
+                      );
+                    }
+                  }
+                });
+            }
           });
-
-          logger.success('Created a new user purchase.');
-
-          return logger.info(
-            `Operation took ${operationEnded - operationStarted}ms.`
-          );
-        } else {
-          response
-            .status(500)
-            .json({ message: 'Error while creating new user purchase.' });
-
-          logger.error('Could not create new user purchase.');
-
-          return logger.info(
-            `Operation took ${operationEnded - operationStarted}ms.`
-          );
-        }
       }
     });
 });
