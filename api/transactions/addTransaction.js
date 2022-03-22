9693de0ae98467927886c777d8617dc43eda70b0
Connor Davis <19422@kloofhigh.co.za>
2022-03-22 11:46:40 +0200

    Fix up transactions logic

--- api/transactions/addTransaction.js
+++ api/transactions/addTransaction.js
@@ -20,20 +20,38 @@ router.post('/', async (request, response) => {
     password: process.env.ROOT_PASSWORD,
   });
 
+  let transactionObject =
+    body.type === 'purchase'
+      ? {
+          user: request.user.id,
+          purchaser: request.user,
+          material: body.material,
+          weight: body.weight,
+          seller: body.seller,
+          price: body.price,
+          type: body.type,
+          date: Date.now(),
+        }
+      : {
+          user: request.user.id,
+          purchaser: body.purchaser,
+          material: body.material,
+          weight: body.weight,
+          seller: request.user,
+          price: body.price,
+          type: body.type,
+          date: Date.now(),
+        };
+
   r.db('threereco')
-    .table('userMaterials')
-    .filter(function (material) {
-      return material('materialName')
-        .eq(body.material.materialName)
-        .and(material('user').eq(request.user.id));
-    })
-    .update({
-      stock:
-        body.type === 'purchase'
-          ? r.row('stock').add(parseFloat(body.weight))
-          : r.row('stock').sub(parseFloat(body.weight)),
-    })
-    .run(connection, (error, result) => {
+    .table('userTransactions')
+    .insert(transactionObject)
+    .run(connection, async (error, result) => {
+      let m2 = moment();
+      let operationEnded =
+        m2.milliseconds() +
+        1000 * (m2.seconds() + 60 * (m2.minutes() + 60 * m2.hours()));
+
       if (error) {
         response.status(500).json({
           message: 'Error while creating new user transaction.',
@@ -46,107 +64,28 @@ router.post('/', async (request, response) => {
           `Operation took ${operationEnded - operationStarted}ms.`
         );
       } else {
-        r.db('threereco')
-          .table('userMaterials')
-          .filter(function (material) {
-            return material('materialName')
-              .eq(body.material.materialName)
-              .and(
-                material('user').eq(
-                  body.type === 'purchase' ? body.seller.id : body.purchaser.id
-                )
-              );
-          })
-          .update({
-            stock:
-              body.type === 'purchase'
-                ? r.row('stock').add(parseFloat(body.weight))
-                : r.row('stock').sub(parseFloat(body.weight)),
-          })
-          .run(connection, (error, result) => {
-            if (error) {
-              response.status(500).json({
-                message: 'Error while creating new user transaction.',
-                error,
-              });
-
-              logger.error(error);
-
-              return logger.info(
-                `Operation took ${operationEnded - operationStarted}ms.`
-              );
-            } else {
-              let transactionObject =
-                body.type === 'purchase'
-                  ? {
-                      user: request.user.id,
-                      purchaser: request.user,
-                      material: body.material,
-                      weight: body.weight,
-                      seller: body.seller,
-                      price: body.price,
-                      type: body.type,
-                      date: Date.now(),
-                    }
-                  : {
-                      user: request.user.id,
-                      purchaser: body.purchaser,
-                      material: body.material,
-                      weight: body.weight,
-                      seller: request.user,
-                      price: body.price,
-                      type: body.type,
-                      date: Date.now(),
-                    };
-
-              r.db('threereco')
-                .table('userTransactions')
-                .insert(transactionObject)
-                .run(connection, async (error, result) => {
-                  let m2 = moment();
-                  let operationEnded =
-                    m2.milliseconds() +
-                    1000 *
-                      (m2.seconds() + 60 * (m2.minutes() + 60 * m2.hours()));
-
-                  if (error) {
-                    response.status(500).json({
-                      message: 'Error while creating new user transaction.',
-                      error,
-                    });
-
-                    logger.error(error);
-
-                    return logger.info(
-                      `Operation took ${operationEnded - operationStarted}ms.`
-                    );
-                  } else {
-                    if (result.inserted >= 1) {
-                      response.status(200).json({
-                        message: 'Created new user transaction successfully.',
-                        data: transactionObject,
-                      });
-
-                      logger.success('Created a new user transaction.');
-
-                      return logger.info(
-                        `Operation took ${operationEnded - operationStarted}ms.`
-                      );
-                    } else {
-                      response.status(500).json({
-                        message: 'Error while creating new user transaction.',
-                      });
-
-                      logger.error('Could not create new user transaction.');
-
-                      return logger.info(
-                        `Operation took ${operationEnded - operationStarted}ms.`
-                      );
-                    }
-                  }
-                });
-            }
+        if (result.inserted >= 1) {
+          response.status(200).json({
+            message: 'Created new user transaction successfully.',
+            data: transactionObject,
           });
+
+          logger.success('Created a new user transaction.');
+
+          return logger.info(
+            `Operation took ${operationEnded - operationStarted}ms.`
+          );
+        } else {
+          response.status(500).json({
+            message: 'Error while creating new user transaction.',
+          });
+
+          logger.error('Could not create new user transaction.');
+
+          return logger.info(
+            `Operation took ${operationEnded - operationStarted}ms.`
+          );
+        }
       }
     });
 });
